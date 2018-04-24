cc9d1109b361134336513304d2fe79e304c4cef4
alterkait <alterkait2001@icloud.com>
2018-04-24 15:08:36 +0300

    Update glidePhysics.cpp

--- glidePhysics.cpp
+++ glidePhysics.cpp
@@ -12,6 +12,12 @@ Glider::Glider(double initial_xposition, double initial_yposition, double initia
   Flag_sprite = flag_sprite;
   Flag_sprite.setscale(0,0);
   
+  ObjectName = vesselName;
+  vesselClass = "Alpha Glider";
   
+  acceleration.set_values(0,0);
+  
+  NewtonianState.FlightState.Position.Set_values(initial_xposition, initial_yposition);
+	NewtonianState.FlightState.Velocity.Set_values(initial_xvelocity, initial_yvelocity);
 }
 
