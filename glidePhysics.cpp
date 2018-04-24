a87a98b92d96b76d17718af4a8a260e1afd42636
alterkait <alterkait2001@icloud.com>
2018-04-24 15:06:54 +0300

    Update glidePhysics.cpp

--- glidePhysics.cpp
+++ glidePhysics.cpp
@@ -1,1 +1,17 @@
-#include 
+#include <string>
+#include <iostream>
+#include <math.h>
+#include "glidePhysics.hpp"
+
+Glider::Glider(double initial_xposition, double initial_yposition, double initial_xvelocity, double initial yvelocity,
+               double initial_theta, double initial_propellant_main, double initial_rcs_propellant)
+{
+  cout << "Simulating Glider Physics" << endl;
+  NewtonianState.current_state = Flight;
+  
+  Flag_sprite = flag_sprite;
+  Flag_sprite.setscale(0,0);
+  
+  
+}
+
